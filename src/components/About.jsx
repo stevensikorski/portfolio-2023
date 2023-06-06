08e32cb1568fa897d88a33a4f81916044e9121f7
stevensikorski <ssikorski03@gmail.com>
2023-06-06 14:21:10 -0400

    Redone home to fix responsiveness issues

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const About = () => {
 	return (
-		<section id='about' className='h-screen container border-b border-gray-300 px-8 lg:px-8 2xl:px-20 pt-[6rem] lg:pt-[3.75rem]'>
-			<p className='justify-start text-2xl lg:text-base'>About</p>
-		</section>
+	<section id='about' className='h-screen container border-b border-gray-300 px-8 lg:px-8 2xl:px-20 pt-[6rem] lg:pt-[3.75rem]'>
+		<p className='justify-start text-2xl lg:text-base'>About</p>
+	</section>
 	)
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,27 +4,31 @@ import { links } from './constants/constants.js'
 
 const Home = () => {
 	return (
-		<section id='home' className='lg:h-screen pb-10 lg:pb-0 container border-b border-gray-300 px-8 lg:px-16 2xl:px-40 pt-[9rem] lg:pt-[3.75rem] flex justify-end lg:justify-between items-center flex-col-reverse lg:flex-row'>
-			<div className='mx-auto lg:pr-16'>
-				<p className='text-8xl pt-24 lg:pt-0 text-center'>Hello there! <span className='animate-waving inline-block'>👋🏻</span></p>
-				<p className='text-4xl lg:px-0 lg:text-lg pt-12 lg:pt-6 lg:pb-6 text-gray-400 text-center'>I'm Steven Sikorski, a Computer Science Undergraduate from New York City. 🗽</p>
-				
-				<ul className='bg-gray-100 rounded px-2 py-2 mt-24 lg:mt-0 flex flex-row text-3xl lg:text-2xl justify-center items-center'>
-					<p className='px-2 text-gray-400 font-thin'>Tech Stack</p>
+		<section id='home' className='h-screen container border-b border-gray-300 px-6 lg:px-8 2xl:px-32 pt-[9rem] lg:pt-[3.75rem] flex flex-col lg:flex-row-reverse justify-start lg:justify-center items-center'>
+			<img src={links[0].picture} className='w-1/2 lg:w-1/3 rounded-[100%] py-[6rem] lg:py-0 transition-all duration-300'></img>	
 
-					<img className='techstack' alt='C++' src='https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg'></img>
-					<img className='techstack' alt='C#' src='https://upload.wikimedia.org/wikipedia/commons/b/bd/Logo_C_sharp.svg'></img>
-					<img className='techstack' alt='Python' src='https://upload.wikimedia.org/wikipedia/commons/archive/c/c3/20220821155028%21Python-logo-notext.svg'></img>
+			<div className='flex flex-col justify-between items-center lg:items-start flex-grow'>
+				<p className='text-9xl lg:text-8xl text-center lg:text-left'>Hello there! <span className='animate-waving inline-block'>👋🏻</span></p>
+				<p className='text-4xl lg:text-lg text-center lg:text-left leading-relaxed mt-8 mb-auto lg:pb-12 text-gray-400'>I'm Steven Sikorski, a Computer Science Undergraduate from New York City. 🗽</p>
 
-					<img className='techstack' alt='HTML' src='https://www.w3.org/html/logo/downloads/HTML5_Badge.svg'></img>
-					<img className='techstack' alt='CSS' src='https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg'></img>
-					<img className='techstack' alt='JavaScript' src='https://www.javascripttutorial.net/wp-content/uploads/2021/04/JavaScript-Tutorial.svg'></img>
-					<img className='techstack' alt='React' src='https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'></img>
-					<img className='techstack' alt='Tailwind' src='https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg'></img>
-				</ul>
+				<div className='flex flex-col lg:flex-row justify-center item-center pb-[6rem] lg:pb-0'>
+					<p className='text-3xl lg:text-lg text-center pb-6 lg:pb-0'><span className='border-b border-gray-300 pb-2 lg:pb-0 lg:border-0'>Tech Stack</span><span className='font-thin text-gray-300 px-4 hidden lg:inline-block'>|</span></p>
+					<ul className='flex justify-center pt-2 lg:pt-0'>
+						<img className='techstack' alt='C++' src='https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg'></img>
+						<img className='techstack' alt='C#' src='https://upload.wikimedia.org/wikipedia/commons/b/bd/Logo_C_sharp.svg'></img>
+						<img className='techstack' alt='Python' src='https://upload.wikimedia.org/wikipedia/commons/archive/c/c3/20220821155028%21Python-logo-notext.svg'></img>
+						<img className='techstack' alt='Lua' src='https://upload.wikimedia.org/wikipedia/commons/c/cf/Lua-Logo.svg'></img>
+
+						<img className='techstack' alt='HTML' src='https://www.w3.org/html/logo/downloads/HTML5_Badge.svg'></img>
+						<img className='techstack' alt='CSS' src='https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg'></img>
+						<img className='techstack' alt='JavaScript' src='https://www.javascripttutorial.net/wp-content/uploads/2021/04/JavaScript-Tutorial.svg'></img>
+						<img className='techstack' alt='React' src='https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'></img>
+						<img className='techstack' alt='Tailwind' src='https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg'></img>
+
+						<img className='techstack' alt='Tailwind' src='https://upload.wikimedia.org/wikipedia/commons/7/7d/Microsoft_.NET_logo.svg'></img>
+					</ul>
+				</div>
 			</div>
-
-			<img className='w-3/4 aspect-square lg:max-w-md bg-white rounded-[100%] border-8 border-gray-100 mx-auto mt-[9rem] lg:mt-0' src={links[0].picture}></img>
 		</section>
 	)
 }
